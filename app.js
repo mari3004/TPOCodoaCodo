1ecc62d6b51f2405cd43a2e3ef33c298bf4f8324
Maribel <mari.3004@hotmail.com>
2023-05-24 22:33:13 -0300

    index, ediciones, etc

---
+++ app.js
@@ -0,0 +1,7 @@
+function carrito(){
+    window.location.href = "https://media.tenor.com/4EAKayO3n_cAAAAC/kitten-cat.gif"
+}
+
+function ingresar(){
+    window.open("https://media.tenor.com/l37il_hI3tEAAAAC/smilecat.gif")
+}
